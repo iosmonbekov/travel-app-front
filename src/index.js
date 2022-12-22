da2d1fde6051b04086650c8be6284e88e9a9d5e6
iosmonbekov <iosmonbekov666@gmail.com>
2022-12-22 17:50:18 +0600

    Added bootstrap and eslint

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import { Routes } from './routes/index';
-import './index.scss';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+import { Routes } from './routes/index'
+import './index.scss'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={Routes.router} />
-  </React.StrictMode>
-);
+	<React.StrictMode>
+		<RouterProvider router={Routes.router} />
+	</React.StrictMode>
+)
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import { router } from './router'
 
 export const Routes = {
-    router,
+	router
 }
