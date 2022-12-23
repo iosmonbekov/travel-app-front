9a7a2a81563c7a943635ac8de381ebe73dbd230a
iosmonbekov <iosmonbekov666@gmail.com>
2022-12-23 17:57:24 +0600

    worked with styles

--- src/pages/sign-in/index.js
+++ src/pages/sign-in/index.js
@@ -13,24 +13,27 @@ export const SignIn = () => {
 					<div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
 						<form>
 							<div className="form-outline mb-4">
-								<label className="form-label" htmlFor="form3Example3">Email address</label>
-								<input type="email" id="form3Example3" className="form-control form-control-lg"
-									placeholder="Enter a valid email address" />
+								<label className="form-label">Email address</label>
+								<input type="email" className="form-control form-control-lg is-invalid" placeholder="Enter a valid email address" />
+								<div className="invalid-feedback">
+									Please provide a valid email.
+								</div>
 							</div>
   
 							<div className="form-outline mb-3">
-								<label className="form-label" htmlFor="form3Example4">Password</label>
-								<input type="password" id="form3Example4" className="form-control form-control-lg"
-									placeholder="Enter password" />
+								<label className="form-label was-validated">Password</label>
+								<input type="password" className="form-control form-control-lg is-valid" placeholder="Enter password" />
 							</div>
   
 							<div className="text-center text-lg-start mt-4 pt-2">
-								<button type="button" className="btn btn-primary btn-lg"
-									style={{'paddingLeft': '2.5rem', 'paddingRight': '2.5rem'}}>Login</button>
-								<p className="small fw-bold mt-2 pt-1 mb-0">Don&apos;t have an account? <a href="#!"
-									className="link-danger">Register</a></p>
+								<button type="button" className="btn btn-primary btn-lg px-5">
+									Login
+								</button>
+								<p className="small fw-bold mt-2 pt-1 mb-0">
+									Don&apos;t have an account? 
+									<a href="#!" className="link-danger mx-2">Register</a>
+								</p>
 							</div>
-  
 						</form>
 					</div>
 				</div>
