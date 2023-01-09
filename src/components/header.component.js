32b39c9873f7a44758e8babcbd849ea630c59e0c
iosmonbekov <iosmonbekov666@gmail.com>
2023-01-09 10:41:39 +0600

    Added logout button

--- src/components/header.component.js
+++ src/components/header.component.js
@@ -2,17 +2,28 @@ import React from 'react'
 import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
+import { useNavigate } from 'react-router-dom'
 
 export const Header = () => {
+	const navigate = useNavigate()
+
+	const logout = () => {
+		localStorage.clear()
+		navigate('/sign-in')
+	}
+
 	return <Navbar bg="light" expand="lg">
 		<Container>
 			<Navbar.Brand>Kettik-Kg</Navbar.Brand>
-			<Navbar.Toggle aria-controls="basic-navbar-nav" />
-			<Navbar.Collapse id="basic-navbar-nav">
+			<Navbar.Toggle />
+			<Navbar.Collapse>
 				<Nav className="me-auto">
 					<Nav.Link>Home</Nav.Link>
 					<Nav.Link>My tours</Nav.Link>
 				</Nav>
+				<button onClick={logout} className='btn btn-light'>
+					Logout
+				</button>
 			</Navbar.Collapse>
 		</Container>
 	</Navbar>
